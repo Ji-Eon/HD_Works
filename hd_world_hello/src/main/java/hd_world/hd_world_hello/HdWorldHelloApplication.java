8b653da7994aeb94feed4812b198493a9d3d6c9c
medikim3551 <medikim3551@gmail.com>
2024-04-11 14:37:33 +0900

    # SpringIO 패키지 설치 및 개발환경 폴더 업로드

---
+++ hd_world_hello/src/main/java/hd_world/hd_world_hello/HdWorldHelloApplication.java
@@ -0,0 +1,13 @@
+package hd_world.hd_world_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdWorldHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdWorldHelloApplication.class, args);
+	}
+
+}
---
+++ hd_world_hello/src/test/java/hd_world/hd_world_hello/HdWorldHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package hd_world.hd_world_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdWorldHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
