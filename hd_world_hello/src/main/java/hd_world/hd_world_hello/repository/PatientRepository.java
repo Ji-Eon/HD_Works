24716c2ff61637f42572d07616ad556f57804ed2
medikim3551 <medikim3551@gmail.com>
2024-04-15 16:44:33 +0900

    # 조건별 검색 PatientRepository 추가

--- hd_world_hello/src/main/java/hd_world/hd_world_hello/repository/PatientRepository.java
+++ hd_world_hello/src/main/java/hd_world/hd_world_hello/repository/PatientRepository.java
@@ -5,12 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     Page<Patient> findAll(Pageable pageable);
+    List<Patient> findByPatientNameContaining(String name);
+    List<Patient> findByPatientIdContaining(String id);
+    List<Patient> findByBirthday(Date birthday);
 
 
 }
